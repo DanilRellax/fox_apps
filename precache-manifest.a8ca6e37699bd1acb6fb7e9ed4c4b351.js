cd7478d9b8ff310da7bd8d52dd5032aac12176ea
DanilRellax <77833594+DanilRellax@users.noreply.github.com>
2021-03-07 01:49:53 +0200

    FOX APP

---
+++ precache-manifest.a8ca6e37699bd1acb6fb7e9ed4c4b351.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bd8d938def95e9fed0a",
+    "url": "./static/css/main.32bb215f.chunk.css"
+  },
+  {
+    "revision": "0bd8d938def95e9fed0a",
+    "url": "./static/js/main.7826983e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3610fda449ccc37f4280",
+    "url": "./static/css/2.58e0904f.chunk.css"
+  },
+  {
+    "revision": "3610fda449ccc37f4280",
+    "url": "./static/js/2.158cb778.chunk.js"
+  },
+  {
+    "revision": "c53e482c55280e18695f533a6fe47bee",
+    "url": "./static/media/coin.c53e482c.png"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "d8dde2c45fabb1c7700b35203f77be7a",
+    "url": "./static/media/click.d8dde2c4.png"
+  },
+  {
+    "revision": "733c91fd1550cc8718455cd9faf17260",
+    "url": "./index.html"
+  }
+];
